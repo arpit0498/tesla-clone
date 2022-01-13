c1ee40bf338091e4e7404192f720ddf060d0de29
arpit gupta <mycontact.arpit@gmail.com>
2022-01-14 03:27:40 +0530

    feat: add useEffect
    provide persistance

--- tesla-clone/src/App.js
+++ tesla-clone/src/App.js
@@ -1,17 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Header';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import Menu from './Menu';
 import HeaderBlock from './HeaderBlock';
 import Login from './Login';
-import { useSelector } from 'react-redux'
-import { selectUser } from './features/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice'
 import Signup from './Signup';
+import TeslaAccount from './TeslaAccount';
+import { auth } from './firebase';
 
 function App() {
   const user = useSelector(selectUser)
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatchEvent(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+        })
+        )
+      } else {
+        //user signed out
+        dispatch(logout())
+      }
+    })
+
+  }, [dispatch])
 
   return (
     <Router>
@@ -32,6 +52,16 @@ function App() {
           <Route exact path="/signup">
             <Signup />
             <Login />
+
+          </Route>
+          <Route exact path='/teslaaccount'>
+            {!user ? <Redirect to='/login' /> : (
+              <>
+                <TeslaAccount isMenuOpen={isMenuOpen}
+                  setIsMenuOpen={setIsMenuOpen} />
+                {isMenuOpen && <Menu />}
+              </>
+            )}
 
           </Route>
         </Switch>
